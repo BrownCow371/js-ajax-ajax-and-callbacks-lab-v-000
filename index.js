e4db473954cbb5d4cfc21beb49c5b255be3f8432
Jamie Brown <jmbrowncow@gmail.com>
2018-09-25 22:45:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ $(document).ready(function (){
 
 });
 
+function buildHTML(response){
+  let responseHTML;
+
+
+  $('#results').html(responseHTML);
+}
+
 function searchRepositories() {
   let searchText = document.getElementById('searchTerms').value;
   let regex = /\s/
@@ -13,6 +20,9 @@ function searchRepositories() {
 
   $.get(searchUrl, function(response){
     // $('#results').html(response);
+
+    // Display the collection of repositories inside the results div. Include repository name, description, and a link to the HTML URL. Also include repository owner login,
+    // repository owner avatar as an image, and a link to the owners profile page. Hint: Pay close attention to the structure of the search results!
     console.log("results", response);
   });
 
